d989d8ad83159b27ba09046f01ae16999dbc6713
Castlabs iOS CI <ios-devs@castlabs.com>
2026-09-02 12:44:29 +0200

    Release SPM package 4.2.0

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let prestoPlayVersion: Version = "4.1.2"
+let prestoPlayVersion: Version = "4.2.0"
 let protobufVersion: Version = "3.13.0"
 let googleCastVersion: Version = "4.8.4"
 let googleIMAiOSVersion: Version = "3.27.4"
